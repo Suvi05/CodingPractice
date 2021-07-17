536de12d2ff74fbfb327889a5a23d2314eab7d08
suvi05 <suv143ak@gmail.com>
2021-07-17 15:20:06 +0530

    Factorial's Program

---
+++ src/com/DSApractice/Recursion/Factorial.java
@@ -0,0 +1,21 @@
+package com.DSApractice.Recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int fact = 0;
+        fact = n * factorial(n - 1);
+        return fact;
+    }
+}
