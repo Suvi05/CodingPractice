bb89e8e2ff32a95ee4d7102153296c6326d52ef8
suvi05 <suv143ak@gmail.com>
2021-07-19 13:27:31 +0530

    Day 12- Homework Question-4 Colorful Number -Using HashSet

--- src/com/Scaler/Homework/Day12/HQuestion4.java
+++ src/com/Scaler/Homework/Day12/HQuestion4.java
@@ -1,6 +1,7 @@
 package com.Scaler.Homework.Day12;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
 
@@ -8,45 +9,33 @@ public class HQuestion4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number : ");
-        long A = sc.nextLong();
+        int A = sc.nextInt();
         System.out.println(colorful(A));
     }
 
-    public static int colorful(long A) {
-
-        char[] ch = Long.toString(A).toCharArray();
-        int len = ch.length;
-        int[] arr = new int[len];
-        for (int i = 0; i < len; i++) {
-            arr[i] = Character.getNumericValue(ch[i]);
+    public static int colorful(int A) {
+        String s = String.valueOf(A);
+        char[] ch = s.toCharArray();
+        int[] arr = new int[ch.length];
+        for (int i = 0; i < ch.length; i++) {
+            arr[i] = Integer.parseInt(String.valueOf(ch[i]));
         }
+        HashSet<Integer> hm = new HashSet<>();
+        int mul = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
 
-
-        int result = 0;
-        HashMap<Integer, Integer> hm = new LinkedHashMap<>();
-        int prod = 1;
-        int i = -1;
-        while (i < ch.length - 1) {
-            i++;
-            prod *= Character.getNumericValue(ch[i]);
-            if (hm.containsKey(prod) == false) {
-                hm.put(prod, Character.getNumericValue(ch[i]));
-            } else {
-                return 1;
+                mul = 1;
+                for (int k = i; k <= j; k++) {
+                    mul = mul * arr[k];
+                }
+                if (hm.contains(mul)) {
+                    return 0;
+                } else {
+                    hm.add(mul);
+                }
             }
         }
-        System.out.println(hm);
-        return result;
+        return 1;
     }
 }
-/*for (int i = 0; i < ch.length; i++) {
-            for(int j=i+1;j<ch.length;j++){
-
-            }
-            prod *= Character.getNumericValue(ch[i]);
-            if (hm.containsKey(prod)) {
-                return 1;
-            } else {
-                hm.put(prod, Character.getNumericValue(ch[i]));
-            }
-        }*/
