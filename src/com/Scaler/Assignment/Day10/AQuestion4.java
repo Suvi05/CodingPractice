5d8d4bb8be440ff5690a55203751ae8353aa8c46
suvi05 <suv143ak@gmail.com>
2021-07-21 20:13:37 +0530

    Day 10- Assignment Question-4 Longest consecutive 1’s
    For Furter reference :
    https://www.geeksforgeeks.org/length-of-longest-consecutive-ones-by-at-most-one-swap-in-a-binary-string/

--- src/com/Scaler/Assignment/Day10/AQuestion4.java
+++ src/com/Scaler/Assignment/Day10/AQuestion4.java
@@ -7,34 +7,59 @@ public class AQuestion4 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the String : ");
         String A = sc.nextLine();
+        System.out.println(solve(A));
+    }
 
-        char[] B = A.toCharArray();
-        int n = B.length;
-        int count0 = 0;
-        int count1 = 0;
-        int large = 0;
-        int largest = 0;
+    public static int solve(String s) {
+        int n = s.length();
+        int cnt_one = 0;
+        int max_cnt = 0;
+        int temp = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            if (B[i] == '0') {
-                count0++;
-            }
-            while (count0 > 0) {
-                if (B[i] == '1') {
-                    count1++;
-                    if (B[i] == '1' && B[i + 1] == '1') {
-                        large++;
-                    }
-                    if (n > large && count1 > large) {
-                        largest = large + 1;
-                    }
-                }
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                cnt_one++;
+                temp++;
+            } else {
+                max_cnt = Math.max(max_cnt, temp);
+                temp = 0;
             }
         }
-        if (count0 < 2) {
-            System.out.println(largest);
-        } else {
-            System.out.println(largest);
+        max_cnt = Math.max(max_cnt, temp);
+
+        // To store cumulative 1's
+        int[] left = new int[n];
+        int right[] = new int[n];
+
+        if (s.charAt(0) == '1') left[0] = 1;
+        else left[0] = 0;
+        if (s.charAt(n - 1) == '1') right[n - 1] = 1;
+        else right[n - 1] = 0;
+        // Counting cumulative 1's from left
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == '1') left[i] = left[i - 1] + 1;
+                // If 0 then start new cumulative
+                // one from that i
+            else left[i] = 0;
         }
+        for (int i = n - 2; i >= 0; i--) {
+            if (s.charAt(i) == '1') right[i] = right[i + 1] + 1;
+            else right[i] = 0;
+        }
+        for (int i = 1; i < n - 1; i++) {
+            // perform step 3 of the approach
+            if (s.charAt(i) == '0') {
+                // step 3
+                int sum = left[i - 1] + right[i + 1];
+                if (sum < cnt_one) max_cnt = Math.max(max_cnt, sum + 1);
+                else max_cnt = Math.max(max_cnt, sum);
+            }
+        }
+        return max_cnt;
     }
 }
+
+// A = "111000"
+//Output 1: 3
+//    A = "111011101"
+//Output 2: 7
