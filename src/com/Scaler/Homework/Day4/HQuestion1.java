92593da7a702959d413fa289f5ad9f7e31a55c50
suvi05 <suv143ak@gmail.com>
2021-07-22 16:45:34 +0530

    Day 14- Homework Question-1 Strictly Smaller and Strictly Greater
    #Revision
    --Rearranged the code's format---

--- src/com/Scaler/Homework/Day4/HQuestion1.java
+++ src/com/Scaler/Homework/Day4/HQuestion1.java
@@ -5,39 +5,42 @@ import java.util.Scanner;
 public class HQuestion1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Number of Elements in Array A : ");
+        System.out.print("Enter the size of Array: ");
         int n = sc.nextInt();
+        System.out.print("Enter the elements in Array :");
         int[] A = new int[n];
-
         for (int i = 0; i < n; i++) {
             A[i] = sc.nextInt();
         }
+        System.out.println(solve(A));
+    }
 
+    public static int solve(int[] A) {
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
 
-        for(int x:A){
-            if(x<min){
-                min=x;
+        for (int x : A) {
+            if (x < min) {
+                min = x;
             }
         }
-        for(int x:A){
-            if(x>max){
-                max=x;
+        for (int x : A) {
+            if (x > max) {
+                max = x;
             }
         }
-
-        System.out.println("Max "+ max);
-        System.out.println("Min "+ min);
-        int k=0;
-        for(int x:A){
-            if(x!=max && x!=min){
+        System.out.println("Max " + max);
+        System.out.println("Min " + min);
+        int k = 0;
+        for (int x : A) {
+            if (x != max && x != min) {
                 k++;
             }
         }
-        System.out.println(k);
+        return k;
     }
 }
-//913, 440, 865,  612, 445, 101, 994, 356, 91, 461, 930, 583,
-//448, 543, 170, 333, 107, 425,72, 73, 172, 416, 899, 826, 659, 561, 314, 25, 110
+
+//12--- 913 440 865  612 445 101 994 356 91 461 930 583 ---> 10 ans
+//17--- 448 543 170 333 107 425 72 73 172 416 899 826 659 561 314 25 110 ---> 15 ans
 
