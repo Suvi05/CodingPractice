1a62aff7ba7c3db0b58c1a04ca717f575af30c01
Suvi05 <suv143ak@gmail.com>
2021-10-10 01:07:29 +0530

    2nd Use Case of "this" Keyword

--- src/com/Pepcoding/OOPs/ThisKeyword2.java
+++ src/com/Pepcoding/OOPs/ThisKeyword2.java
@@ -10,26 +10,45 @@ public class ThisKeyword2 {
             this.brand = brand;
             this.model = model;
             this.ram = ram;
-            System.out.println("Address in Constructor : " + this);
-            //This address is the address in HEAP MEMORY where this Object has been stored
+            //this.display();
+            //Even if we will just use display(), it will work but this.display() is better practice
         }
 
         public void display() {
-            System.out.println("Brand : " + brand);
-            System.out.println("Model : " + model);
-            System.out.println("RAM : " + ram + " GB");
+            System.out.println("Brand : " + brand); //Using this.brand is better practice
+            System.out.println("Model : " + model); //Using this.model is better practice
+            System.out.println("RAM : " + ram + " GB"); //Using this.ram is better practice
             System.out.println("---------------------------");
         }
+
+        public void display1() {
+            this.brand(); //Using this.brand is better practice
+            this.model();//Using this.model is better practice
+            this.ram();//Using this.ram is better practice
+            System.out.println("+++++++++++++++++++++++++++");
+        }
+
+        public void brand() {
+            System.out.println("Brand (this): " + this.brand);
+            //Using this.brand is better practice
+        }
+
+        public void model() {
+            System.out.println("Model (this): " + this.model);
+            //Using this.model is better practice
+        }
+
+        public void ram() {
+            System.out.println("RAM (this): " + this.ram);
+            //Using this.ram is better practice
+        }
+
     }
 
     public static void main(String[] args) {
         Phone p1 = new Phone("Samsung", "Galaxy", 12);
-        System.out.println("Address in Main Method : " + p1);
-        //This address is the address in HEAP MEMORY where this Object has been stored
-        p1.display();
-
+        p1.display1();
         Phone p2 = new Phone("Samsung", "Galaxy Grand Prime", 8);
-        System.out.println("Address in Main Method : " + p2);
-        p2.display();
+        p2.display1();
     }
 }
