50c50c2d7211fbbed401248802826bdcfb84a351
Suvi05 <suv143ak@gmail.com>
2021-10-10 10:09:30 +0530

    3nd Use Case of "this" Keyword "this" can be used to invoke current class constructor. //Constructor Chaining

--- src/com/Pepcoding/OOPs/ThisKeyword3.java
+++ src/com/Pepcoding/OOPs/ThisKeyword3.java
@@ -21,10 +21,41 @@ public class ThisKeyword3 {
         }
     }
 
+    public static class Rectangle {
+        int x = 0, y = 0, h = 0, w = 0;
+
+        public Rectangle() {
+            this(1, 1);
+        }
+
+        public Rectangle(int h, int w) {
+            this(0, 0, h, w);
+        }
+
+        public Rectangle(int x, int y, int h, int w) {
+            this.x = x;
+            this.y = y;
+            this.h = h;
+            this.w = w;
+            this.printDimension();
+            System.out.println();
+        }
+
+        public void printDimension() {
+            System.out.println("Origin at : " + this.x + " , " + this.y);
+            System.out.println("Dimension : " + this.h + " " + this.w);
+        }
+    }
+
     public static void main(String[] args) {
         Sum s0 = new Sum();
         Sum s1 = new Sum(10, 20);
         Sum s2 = new Sum(10, 20, 30);
         Sum s3 = new Sum(10, 20, 30, 40);
+        System.out.println("************************");
+
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Rectangle(10, 20);
+        Rectangle r3 = new Rectangle(10, 20, 30, 40);
     }
 }
