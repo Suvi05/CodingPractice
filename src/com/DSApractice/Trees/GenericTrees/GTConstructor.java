cb883564ff8485474f7e0eb58e2f1243e86b2dd4
Sumit Verma <suv143ak@gmail.com>
2026-03-17 17:23:04 +0530

    GT1_GenericTreeIntro

--- src/com/DSApractice/Trees/GenericTrees/GTConstructor.java
+++ src/com/DSApractice/Trees/GenericTrees/GTConstructor.java
@@ -1,6 +1,33 @@
 package com.DSApractice.Trees.GenericTrees;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class GTConstructor {
+    private static class Node {
+        int val;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
     public static void main(String[] args) {
+        int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+        Node root;
+
+        Stack<Node> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1) {
+                st.pop();
+            } else {
+                Node t = new Node();
+                t.val = arr[i];
+
+                if (st.size() > 0) {
+                    st.peek().children.add(t);
+                } else {
+                    root = t;
+                }
+                st.push(t);
+            }
+        }
     }
 }
